2fb824a986051c813debe0fc09d6e471f855181c
ratkoski <ratkoski@wp.pl>
2023-09-26 20:29:52 +0200

    nareszcie działa

---
+++ test.py
@@ -0,0 +1,15 @@
+from tictac import display_board
+from tictac import player_position
+from tictac import player_marker, place_marker, win_check,player_input
+
+
+test_board = ['#','X','O','X','O','X','O','X','O','X']
+#place_marker(test_board,'X',8)
+display_board(test_board)
+
+#player_position()
+
+#player_marker()
+#win_check(test_board,'X')
+#player_input()
+
